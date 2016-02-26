61db8c603860b00d8f5b287be0fccd9abd71c1d3
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-02-26 04:31:33 +0400

    support auto update

--- update.php
+++ update.php
@@ -3,7 +3,6 @@ namespace infrajs\imager;
 use infrajs\path\Path;
 
 require_once(__DIR__.'/../../../vendor/autoload.php');
-require_once(__DIR__.'/../path/install.php');
 
 Path::mkdir(Imager::$conf['cache']);
 Path::mkdir(Imager::$conf['cache'].'remote/');
